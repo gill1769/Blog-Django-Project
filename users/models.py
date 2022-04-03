45a8552523023ce0de4e74b36b5ff1bcd68fb4df
Harjinder Gill <harjindergill1769@gmail.com>
2022-04-03 17:21:44 -0400

    API

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+#from PIL import Image
 
 
 class Profile(models.Model):
@@ -13,9 +13,9 @@ class Profile(models.Model):
     def save(self):
         super().save()
 
-        img = Image.open(self.image.path)
+        #img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        #if img.height > 300 or img.width > 300:
+         #   output_size = (300, 300)
+          #  img.thumbnail(output_size)
+           # img.save(self.image.path)
